b372fea920ef653bbf3dd52d50d1c5ad0d2233fd
NB <nadinrus@gmail.com>
2024-09-22 15:00:53 +0300

    Correction 1

--- lab1/urls.py
+++ lab1/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('detail/<int:detail_id>/', views.detail, name='detail'),
-    # path('orders/', views.get_order, name='orders'),
-
-    path('order/', views.order_view, name='order'),
-    path('add_to_order/<int:service_id>/', views.add_to_order, name='add_to_order'),
+    path('order/<int:order_id>/', views.order, name='order'),
 ]
