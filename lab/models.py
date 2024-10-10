8f3440bbead09c8ade7ebb6aa375c95d5cf6d7f5
NB <nadin03rus@gmail.com>
2024-10-10 09:41:51 +0300

    Удалила лишнее

--- lab/models.py
+++ lab/models.py
@@ -5,28 +5,33 @@ from django.contrib.auth.models import User
 # Модель "Космические объекты"
 class SpaceObject(models.Model):
     class Meta:
-        db_table = 'SpaceObject'
+        db_table = 'object'
+        verbose_name = 'Космический объект'
+        verbose_name_plural = 'Космические объекты'
 
-    name = models.CharField(max_length=127, verbose_name="Наименование")
-    uncrewed_spacecraft = models.CharField(max_length=127, blank=True, verbose_name="АМС")
+    name = models.CharField(max_length=127, verbose_name="Космический объект")
     description = models.TextField(verbose_name="Описание")
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True,
-                                verbose_name='Цена')
     is_active = models.BooleanField(default=True,
                                     verbose_name="Статус (активен)")
-    scheduled_at = models.DateField(null=True, blank=True,
-                                    verbose_name="Запланированная дата")
     image_url = models.URLField(verbose_name="URL к изображению")
 
     def __str__(self):
         return self.name
 
 
-# Модель "АМС"
+# Модель "Полёт к космическим объектам"
 class UncrewedSpacecraft(models.Model):
     class Meta:
-        db_table = 'UncrewedSpacecraft'
+        db_table = 'spacecraft'
+        verbose_name = 'Автоматическая межпланетная станция'
+        verbose_name_plural = 'Автоматические межпланетные станции'
 
+    spacecraft_name = models.CharField(max_length=127,
+                                       verbose_name='Название АМС')
+    spacecraft_description = models.TextField(null=True, blank=True,
+                                              verbose_name="Описание АМС")
+    scheduled_at = models.DateField(null=True, blank=True,
+                                    verbose_name="Запланированная дата")
     STATUS_CHOICES = (
         ('draft', 'Черновик'),
         ('deleted', 'Удалён'),
@@ -42,23 +47,23 @@ class UncrewedSpacecraft(models.Model):
                                      verbose_name="Дата формирования")
     completed_at = models.DateTimeField(null=True, blank=True,
                                         verbose_name="Дата завершения")
-    space_objects = models.ManyToManyField(SpaceObject, through='Flight')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
                                 blank=True,
-                                related_name='created_uncrewed_spacecrafts',
-                                verbose_name="Создатель заявки")
-    moderator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Модератор", null=True, blank=True)
+                                related_name='created_spacecrafts',
+                                verbose_name="Создатель")
+    moderator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                  blank=True,
+                                  related_name='moderated_spacecrafts',
+                                  verbose_name="Модератор")
 
     def __str__(self):
-        return f"Заявка № {self.pk} ({self.status})"
-
-    #def __str__(self):
-    #    return f"Заявка № {self.pk} ({self.status})"
+        return f"Полёт № {self.pk} ({self.status})"
 
     @staticmethod
     def get_draft_request(user):
-        return UncrewedSpacecraft.objects.filter(user=user, status='draft').first()
+        return UncrewedSpacecraft.objects.filter(user=user,
+                                                 status='draft').first()
 
     def get_object_count(self):
         return self.space_objects.count()
@@ -69,18 +74,23 @@ class UncrewedSpacecraft(models.Model):
 
 
 # Модель связи "Полёт к космическим объектам" - "Космические объекты"
-class Flight(models.Model):
-    uncrewed_spacecraft = models.ForeignKey(UncrewedSpacecraft, on_delete=models.CASCADE)
-    space_object = models.ForeignKey(SpaceObject, on_delete=models.CASCADE)
-    #quantity = models.PositiveIntegerField(verbose_name="Количество услуг")
-    # order = models.PositiveIntegerField(verbose_name="Порядок")
-    # is_main = models.BooleanField(default=False, verbose_name="Главный")
+class FlightSpaceObject(models.Model):
+    spacecraft = models.ForeignKey(UncrewedSpacecraft,
+                                   on_delete=models.CASCADE,
+                                   related_name='space_objects')
+    space_object = models.ForeignKey(SpaceObject, on_delete=models.CASCADE,
+                                     related_name='spacecrafts')
     create_date = models.DateField(null=True, blank=True,
                                    verbose_name="Дата создания заявки")
+    is_priority = models.BooleanField(default=False,
+                                      verbose_name="Приоритетный")
 
     class Meta:
-        db_table = 'Flight'
-        unique_together = ('uncrewed_spacecraft', 'space_object')
+        db_table = 'spacecraft_spaceobject'
+        unique_together = ('spacecraft', 'space_object')
+        verbose_name = 'Объект полёта'
+        verbose_name_plural = 'Объекты полёта'
 
     def __str__(self):
-        return str(self.id)
+        # return str(self.id)
+        return f'{self.spacecraft} - {self.space_object}'
